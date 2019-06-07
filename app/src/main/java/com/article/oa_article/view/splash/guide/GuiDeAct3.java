63a154bd2af8ed67e562ab217279bef07de2b2ad
wuliang6661 <wuliang6661@163.com>
2019-06-07 10:09:44 +0800

    哈哈哈哈或

--- app/src/main/java/com/article/oa_article/view/splash/guide/GuiDeAct3.java
+++ app/src/main/java/com/article/oa_article/view/splash/guide/GuiDeAct3.java
@@ -1,10 +1,12 @@
 package com.article.oa_article.view.splash.guide;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.article.oa_article.R;
@@ -24,6 +26,8 @@ public class GuiDeAct3 extends BaseActivity {
     LinearLayout back;
     @BindView(R.id.next)
     Button next;
+    @BindView(R.id.bg_img)
+    ImageView bgImg;
 
     @Override
     protected int getLayout() {
@@ -37,6 +41,10 @@ public class GuiDeAct3 extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.guide3_bg);
+//        Bitmap b = Bitmap.createScaledBitmap(image, 1500, 2000, false);
+//        bgImg.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        bgImg.setImageBitmap(b);
         back.setOnClickListener(view -> finish());
         next.setOnClickListener(view -> gotoActivity(GuiDe4Act.class, false));
     }
