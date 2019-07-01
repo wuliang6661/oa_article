911af2486fc9e6df22826fce8981836fb884d229
wuliang6661 <wuliang6661@163.com>
2019-07-01 14:27:27 +0800

    哈哈哈哈或

--- app/src/main/java/com/article/oa_article/module/task_allot/Task_allotFragment.java
+++ app/src/main/java/com/article/oa_article/module/task_allot/Task_allotFragment.java
@@ -359,7 +359,11 @@ public class Task_allotFragment extends MVPBaseFragment<Task_allotContract.View,
             @Override
             public void convert(LGViewHolder holder, AddTaskRequest.OrderTasksBean s, int position) {
                 holder.setText(R.id.task_name, s.getTaskName());
-                holder.setText(R.id.task_person_name, s.getNickName().length() > 3 ? s.getNickName().substring(0, 3) + "..." : s.getNickName());
+                if (!StringUtils.isEmpty(s.getNickName())) {
+                    holder.setText(R.id.task_person_name, s.getNickName().length() > 3 ? s.getNickName().substring(0, 3) + "..." : s.getNickName());
+                } else {
+                    holder.setText(R.id.task_person_name, s.getNickName());
+                }
                 holder.setText(R.id.task_shiji_num, s.getActualNum() == 0 ? "--" : s.getActualNum() + "");
                 if (StringUtils.isEmpty(s.getUnit())) {
                     holder.setText(R.id.task_jihua_num, s.getPlanNum() == 0 ? "--" : s.getPlanNum() + "");
